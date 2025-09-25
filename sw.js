e1a350db1612ccbb903a0a5b95ed6b177d9e4140
grabowskip1 <136899626+grabowskip1@users.noreply.github.com>
2025-09-25 16:28:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('v1').then(c=>c.addAll(['/','/index.html','/manifest.json','/icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(new URL(e.request.url).pathname).then(r=>r||caches.match('/index.html')))));
+// Prosty service worker dla GitHub Pages (cache statyków)
+const CACHE = "uz-przerwy-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  // Network-first dla Workera, cache-first dla statyków
+  const isWorkerAPI = /\/parseUZ(\?|$)/.test(new URL(req.url).pathname);
+  if (isWorkerAPI) {
+    e.respondWith(
+      fetch(req).then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(req, clone)).catch(()=>{});
+        return r;
+      }).catch(() => caches.match(req))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return r;
+    }))
+  );
+});
